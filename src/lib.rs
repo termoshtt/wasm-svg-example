3744edcc309f30335cfcc589ad9a5148ab351bb0
Toshiki Teramura <toshiki.teramura@gmail.com>
2019-08-10 16:17:35 +0900

    Try SVG output

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,25 @@
 use wasm_bindgen::prelude::*;
 
-// Called by our JS entry point to run the example
 #[wasm_bindgen(start)]
 pub fn run() -> Result<(), JsValue> {
-    // Use `web_sys`'s global `window` function to get a handle on the global
-    // window object.
     let window = web_sys::window().expect("no global `window` exists");
     let document = window.document().expect("should have a document on window");
     let body = document.body().expect("document should have a body");
 
-    // Manufacture the element we're gonna append
-    let val = document.create_element("p")?;
-    val.set_inner_html("Hello from Rust!");
+    let svg = document.create_element_ns(Some("http://www.w3.org/2000/svg"), "svg")?;
+    svg.set_attribute("width", "100")?;
+    svg.set_attribute("hight", "100")?;
+    svg.set_attribute("viewBox", "0 0 100 100")?;
+    svg.set_attribute("xmlns", "http://www.w3.org/2000/svg")?;
 
-    body.append_child(&val)?;
+    let circle = document.create_element("circle")?;
+    circle.set_attribute("cx", "50")?;
+    circle.set_attribute("cy", "50")?;
+    circle.set_attribute("r", "20")?;
+    circle.set_attribute("stroke", "black")?;
+    circle.set_attribute("fill", "red")?;
+    svg.append_child(&circle)?;
+    body.append_child(&svg)?;
 
     Ok(())
 }
